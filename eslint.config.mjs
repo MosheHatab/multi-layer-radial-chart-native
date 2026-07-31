dd6fc8c82643d2b106834e033349bc9250bf38a0
MosheHatab <44439582+MosheHatab@users.noreply.github.com>
2026-07-31 18:36:09 +0300

    minor fixes + readme updates + update version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import unusedImports from "eslint-plugin-unused-imports";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-	{ ignores: ["lib/**", "coverage/**", "example/**", "node_modules/**"] },
+	{ ignores: ["lib/**", "coverage/**", "example/**", "node_modules/**", "scripts/**"] },
 	js.configs.recommended,
 	...tseslint.configs.recommended,
 	{
